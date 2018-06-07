4da705e112a015adb9dfdb4d7c9ed2f62569be96
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-06-07 17:15:56 -0300

    Readme + Fluxograma + Identação

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cifra_de_cesar_ProdutoFinal
+![Fluxograma Produto_Cézar_ProdutoFinal](Produto_Cézar.jpeg)
+
+##Tarefas
+  1. Solicitar palavra ao usuário: criar prompt;
+  2. Usuário não pode deixar prompt vazio ou inserir número;
+
+###Codificar string fornecida pelo usuário - Com Cifra de César e chave 33:
+    1. Criar a função cipher;
+    2. Descobrir o número Ascii de cada letra da string = numberAscii;
+    3. Descobrir o numero Ascii de cada letra codificada = codeNumberAscii;
+    4. Retornar as letras codificadas = stringCode.
+
+####Descodificar string codificada - Com Cifra de César e chave 33:
+        1. Criar a função decipher;
+        2. Descobrir o número Ascii de cada letra da string = numberAscii;
+        3. Descobrir o numero Ascii de cada letra descodificada = codeNumberAscii;
+        4. Retornar as letras descodificadas = stringCode.
--- app.js
+++ app.js
@@ -3,17 +3,13 @@ while (stringUser === '') { //| typeof parseInt(stringUser) === 'number'){
   alert('Insira apenas letras');
   stringUser = prompt('Insira a palavra que você quer codificar com a Cifra de César:');
 }
-
-//função para codificar a string
 function cipher (stringUser) {
   var numberAscii = [];
   var codeNumberAscii = [];
   var stringCode = [];
-//for para descobrir o número Ascci de cada letra da string
   for (i = 0; i < stringUser.length; i++){
     numberAscii.push(stringUser.charCodeAt(i));
   }
-//for para descobrir o numero Ascii de cada letra codificada
   for (j = 0; j < numberAscii.length; j++){
     if (96 < numberAscii[j] < 123){
       codeNumberAscii.push((numberAscii[j] - 97 + 7) % 26 + 97);
@@ -24,43 +20,28 @@ function cipher (stringUser) {
   for (h = 0; h < codeNumberAscii.length; h++){
     stringCode.push(String.fromCharCode(codeNumberAscii[h]));
   }
-
   return stringCode.join('');
 }
-
 var resultCipher = cipher(stringUser);
-
-
-
-
 function decipher (resultCipher) {
   var numberAscii = [];
   var codeNumberAscii = [];
   var stringCode = [];
-//for para descobrir o número Ascci de cada letra da string
   for (i = 0; i < resultCipher.length; i++){
     numberAscii.push(resultCipher.charCodeAt(i));
   }
-  console.log(numberAscii);
-//for para descobrir o numero Ascii de cada letra descodificada
   for (j = 0; j < numberAscii.length; j++){
-
     if (96 < numberAscii[j] < 123){
       codeNumberAscii.push((numberAscii[j] - 97 - 7) % 26 + 97);
     }
     else if (numberAscii[j] > 64 && numberAscii[j] < 91){
       codeNumberAscii.push((numberAscii[j] - 65 - 7) % 26 + 65);
     }
-
  }
-  console.log(codeNumberAscii);
-
   for (h = 0; h < codeNumberAscii.length; h++){
     stringCode.push(String.fromCharCode(codeNumberAscii[h]));
   }
-
   return stringCode.join('');
 }
-
 document.write('Sua palavra codificada é ' + cipher(stringUser));
 document.write(' e sua palavra descodificada é ' + decipher(resultCipher) + '!');
