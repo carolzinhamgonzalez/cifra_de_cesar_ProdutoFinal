3dba6409bc18a6406b7c01fa8877cdb96e8aed8b
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-06-07 16:52:47 -0300

    Descodificando letras minúsculas + funções

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ while (stringUser === '') { //| typeof parseInt(stringUser) === 'number'){
 }
 
 //função para codificar a string
-//function cipher (stringUser) {
+function cipher (stringUser) {
   var numberAscii = [];
   var codeNumberAscii = [];
   var stringCode = [];
@@ -13,26 +13,54 @@ while (stringUser === '') { //| typeof parseInt(stringUser) === 'number'){
   for (i = 0; i < stringUser.length; i++){
     numberAscii.push(stringUser.charCodeAt(i));
   }
-console.log(numberAscii);
 //for para descobrir o numero Ascii de cada letra codificada
   for (j = 0; j < numberAscii.length; j++){
     if (96 < numberAscii[j] < 123){
       codeNumberAscii.push((numberAscii[j] - 97 + 7) % 26 + 97);
-    } else if ((64 < numberAscii[j] < 91)){
+    } else if (64 < numberAscii[j] < 91){
       codeNumberAscii.push((numberAscii[j] - 65 + 7) % 26 + 65);
     }
-  //  if (64 < numberAscii[j] < 91){
-  //    codeNumberAscii.push((numberAscii[j] - 65 + 7) % 26 + 65);
-  //  } else if (96 < numberAscii[j] < 123){
-  //    codeNumberAscii.push((numberAscii[j] - 97 + 7) % 26 + 97);
-  //  }
+  }
+  for (h = 0; h < codeNumberAscii.length; h++){
+    stringCode.push(String.fromCharCode(codeNumberAscii[h]));
   }
 
-//}
-console.log(codeNumberAscii);
+  return stringCode.join('');
+}
+
+var resultCipher = cipher(stringUser);
+
+
+
+
+function decipher (resultCipher) {
+  var numberAscii = [];
+  var codeNumberAscii = [];
+  var stringCode = [];
+//for para descobrir o número Ascci de cada letra da string
+  for (i = 0; i < resultCipher.length; i++){
+    numberAscii.push(resultCipher.charCodeAt(i));
+  }
+  console.log(numberAscii);
+//for para descobrir o numero Ascii de cada letra descodificada
+  for (j = 0; j < numberAscii.length; j++){
+
+    if (96 < numberAscii[j] < 123){
+      codeNumberAscii.push((numberAscii[j] - 97 - 7) % 26 + 97);
+    }
+    else if (numberAscii[j] > 64 && numberAscii[j] < 91){
+      codeNumberAscii.push((numberAscii[j] - 65 - 7) % 26 + 65);
+    }
+
+ }
+  console.log(codeNumberAscii);
 
   for (h = 0; h < codeNumberAscii.length; h++){
     stringCode.push(String.fromCharCode(codeNumberAscii[h]));
   }
 
-console.log(stringCode);
+  return stringCode.join('');
+}
+
+document.write('Sua palavra codificada é ' + cipher(stringUser));
+document.write(' e sua palavra descodificada é ' + decipher(resultCipher) + '!');
