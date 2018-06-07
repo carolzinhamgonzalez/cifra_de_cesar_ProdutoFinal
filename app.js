7f449dcc283c30b043081cdeb8a0c95eb04cd474
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-06-07 14:54:11 -0300

    Codificando letras maiúsculas

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ while (stringUser === '') { //| typeof parseInt(stringUser) === 'number'){
   alert('Insira apenas letras');
   stringUser = prompt('Insira a palavra que você quer codificar com a Cifra de César:');
 }
+
+//função para codificar a string
+//function cipher (stringUser) {
+  var numberAscii = [];
+  var codeNumberAscii = [];
+  var stringCode = [];
+//for para descobrir o número Ascci de cada letra da string
+  for (i = 0; i < stringUser.length; i++){
+    numberAscii.push(stringUser.charCodeAt(i));
+  }
+console.log(numberAscii);
+//for para descobrir o numero Ascii de cada letra codificada
+  for (j = 0; j < numberAscii.length; j++){
+    codeNumberAscii.push((numberAscii[j] - 65 + 7) % 26 + 65);
+  }
+//}
+console.log(codeNumberAscii);
+
+  for (h = 0; h < codeNumberAscii.length; h++){
+    stringCode.push(String.fromCharCode(codeNumberAscii[h]));
+  }
+
+console.log(stringCode);
